1f8db98e80703fe39823c616b5f700c08e71b117
Mohanapriya2828 <mohanapriya.jayabalan@psiog.com>
2025-10-07 00:41:16 +0530

    Main function implementation commit

--- Program.cs
+++ Program.cs
@@ -1,17 +1,43 @@
-using hangman.model;
-using hangman.view;
-using hangman.controller;
+using System;
+using Hangman.Model;
+using Hangman.Controller;
+using Hangman.View;
 
-class program
+namespace Hangman
 {
-    static void Main()
+    class Program
     {
-        string wordcsv = "CSV/words.csv";
+        static void Main(string[] args)
+        {
+            string userCsv = "CSV/users.csv";
+            string wordCsv = "CSV/words.csv";
+            UserRepository userRepo = new UserRepository(userCsv);
+            WordRepository wordRepo = new WordRepository(wordCsv);
+            LoginView loginView = new LoginView();
+            GameView gameView = new GameView();
+            LoginController loginController = new LoginController(loginView, userRepo);
+            GuessHandler guessRule = GuessRulesFactory.GetRule("simple");
+            GameController gameController = new GameController(gameView, wordRepo, guessRule);
+            AdminController adminController = new AdminController(wordRepo);
 
-        wordrepository repo = new wordrepository(wordcsv);
-        gameview view = new gameview();
-        admincontroller admin = new admincontroller(repo, view);
+            User loggedInUser = null;
+            while (loggedInUser == null)
+                loggedInUser = loginController.LoginWithRole();
 
-        admin.showmenu();
+            if (loggedInUser.Role.ToLower() == "admin")
+                adminController.ShowMenu();
+            else
+            {
+                bool playAgain = true;
+                while (playAgain)
+                {
+                    gameController.PlayGame();
+                    Console.Write("Do you want to play again? (y/n): ");
+                    playAgain = Console.ReadLine().ToLower() == "y";
+                }
+            }
+
+            Console.WriteLine("Thanks for playing Hangman!");
+        }
     }
 }
